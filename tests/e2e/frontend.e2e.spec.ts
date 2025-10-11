fac40c4a39420f3075c26744f3110901274b9724
patelsaajan <saajanpatel@hotmail.com>
2025-10-11 19:00:42 +0100

    feat: changing local host to 400

--- tests/e2e/frontend.e2e.spec.ts
+++ tests/e2e/frontend.e2e.spec.ts
@@ -9,7 +9,7 @@ test.describe('Frontend', () => {
   })
 
   test('can go on homepage', async ({ page }) => {
-    await page.goto('http://localhost:3000')
+    await page.goto('http://localhost:4000')
 
     await expect(page).toHaveTitle(/Payload Website Template/)
 
